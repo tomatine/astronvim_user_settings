14a625d3e31aab9f5b5d7ea5be3a14fc9e573616
tomatine <tomatine86@gmail.com>
2023-05-25 14:09:57 +0900

    add plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,47 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "github/copilot.vim",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function() vim.g.copilot_no_tab_map = true end,
+  },
+  {
+    "jalvesaq/Nvim-R",
+    lazy = false,
+  },
+  {
+    "hkupty/iron.nvim",
+    lazy = false,
+    config = function()
+      require('iron.core').setup {
+        config  = {
+          shoud_map_plug = false,
+          scratch_repl = true,
+          repl_definition = {
+            python = {
+              command = { "ipython" },
+              format = require("iron.fts.common").bracketed_paste,
+            }
+          },
+          repl_open_cmd = require('iron.view').split.vertical.botright('40%'),
+        },
+        keymaps = {
+          send_motion = "<space>sc",
+          visual_send = "<space>sc",
+          send_file = "<space>sf",
+          send_line = "<space>sl",
+          send_mark = "<space>sm",
+          mark_motion = "<space>mc",
+          mark_visual = "<space>mc",
+          remove_mark = "<space>md",
+          cr = "<space>s<cr>",
+          interrupt = "<space>s<space>",
+          exit = "<space>sq",
+          clear = "<space>cl",
+        },
+      }
+    end
+  },
 }
